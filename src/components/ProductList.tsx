39b54530da49c2d5e5c194f1a36d1e82fbd00ac5
Anas-Faiz <anasfaiz0811@gmail.com>
2025-12-07 12:31:17 -0800

    productList optimised

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,7 +3,10 @@ import ProductItems from "./ProductItems"
 function ProductList (){
     return (
         <div>
-            productList
+            <div className="flex justify-between p-1 m-1">
+                <p className="font-bold">Products</p>
+                <button className="border font-bold p-1 bg-yellow-400">Cart</button>
+            </div>
             <ProductItems/>
         </div>
 
